c80dc9b92fa46a7e1b45f7b09c4888792815ef91
thomas-lecouturier <thomas.lecouturier@gmail.com>
2021-09-21 18:23:34 +0200

    progression du theme onews

--- wp-content/themes/onews/functions.php
+++ wp-content/themes/onews/functions.php
@@ -10,3 +10,20 @@ function onews_register_styles()
 // On associe la fonction onews_register_styles à l'action wp_enqueue_scripts
 // Ça veut dire que lorsque WP exécute la fonction do_action('wp_enqueue_scripts'), ça exécute la fonction onews_register_styles
 add_action('wp_enqueue_scripts', 'onews_register_styles');
+
+function onews_after_setup_theme()
+{
+    // On demande à WP d'ajouter lui-mème la balise <title>
+    add_theme_support( 'title-tag' );
+
+    // Facultatif car on utilise ensuite register_nav_menus qui induit déjà la prise en charge de menus
+    // Cette fonction déclare à WordPress que le thème prend en charge des menus
+    add_theme_support( 'menus' );
+
+    // On lui donne un tableau indexé avec en clé les noms des zones de menus et en valeurs leurs noms à afficher dans l'admin
+    register_nav_menus([
+        'footer' => 'Menu en bas à gauche du site',
+    ]);
+}
+
+add_action('after_setup_theme', 'onews_after_setup_theme');
--- wp-content/themes/onews/header.php
+++ wp-content/themes/onews/header.php
@@ -7,7 +7,7 @@
 <body <?php body_class(); ?>>
     <div class="wrapper">
         <header id="welcome">
-            <a href="index.php" id="welcome-logo">O'clock students news</a>
+            <a href="<?php echo home_url(); ?>" id="welcome-logo"><?php bloginfo('name'); ?> <?php bloginfo('description'); ?></a>
             <div id="welcome-message">
                 <h1>Latest news <span class="detail">from our students</span></h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam earum vero reiciendis molestiae corporis eveniet laborum quibusdam explicabo? Error sapiente quidem corporis mollitia rerum id.</p>
