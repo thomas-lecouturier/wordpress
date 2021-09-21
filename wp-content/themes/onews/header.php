279e0c40d12d71f2bf2acb4d1dbe0696021a3e69
thomas-lecouturier <thomas.lecouturier@gmail.com>
2021-09-21 23:17:19 +0200

    last commit

--- wp-content/themes/onews/header.php
+++ wp-content/themes/onews/header.php
@@ -12,10 +12,10 @@
                 <h1>Latest news <span class="detail">from our students</span></h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam earum vero reiciendis molestiae corporis eveniet laborum quibusdam explicabo? Error sapiente quidem corporis mollitia rerum id.</p>
             </div>
-            <nav id="menu">
-                <a href="#">Plan du site</a>
-                <a href="#">Mentions légales</a>
-                <a href="#">Contact</a>
-            </nav>
+            <?php wp_nav_menu([
+                'theme_location' => 'footer', // On précise le nom d'emplcement du menu. C'est un nom normalement décidé dans functions.php
+                'container' => 'nav', // Précise que le menu est dans une nav et pas dans une div
+                'container_id' => 'menu',
+            ]); ?>
         </header>
         <main id="content">
