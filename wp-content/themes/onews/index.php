f5aa8aed1619c47bbfce99a9b8b2eb10c63b8bbf
thomas-lecouturier <thomas.lecouturier@gmail.com>
2021-09-21 12:30:10 +0200

    affichage dynamique des articles, des catégories

--- wp-content/themes/onews/index.php
+++ wp-content/themes/onews/index.php
@@ -2,46 +2,25 @@
 
 <h2>Latest news</h2>
 <section id="posts">
-    <article>
-        <span class="category-badge category-badge--news">News</span>
-        <h3 class="post-title">Lorem ipsum dolor sit amet</h3>
-        <div class="meta">
-            <img src="../images/icon-john.png" alt="">
-            <span><strong>John Marchill</strong> le 21 Juillet 2019</span>
-        </div>
-        <p class="excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor illum, placeat corporis expedita fugit impedit. Aperiam odio eveniet saepe, neque consequuntur nisi voluptatibus! Pariatur!</p>
-        <a href="#" class="post-more">Continue reading</a>
-    </article>
-    <article>
-        <span class="category-badge category-badge--team">Team</span>
-        <h3 class="post-title">Le pair programming pour les n00bs</h3>
-        <div class="meta">
-            <img src="../images/icon-dar.png" alt="">
-            <span><strong>Paul Darson</strong> le 2 Août 2019</span>
-        </div>
-        <p class="excerpt">Le pair programming pour les dévs, c'est un peu comme certains fromages : c'est mieux à deux.</p>
-        <a href="#" class="post-more">Continue reading</a>
-    </article>
-    <article>
-        <span class="category-badge category-badge--work">Work</span>
-        <h3 class="post-title">Le travail : pourquoi ce n'est jamais naturel ?</h3>
-        <div class="meta">
-            <img src="../images/icon-tr.png" alt="">
-            <span><strong>Eric Truc</strong> le 6 Septembre 2019</span>
-        </div>
-        <p class="excerpt">Le terme "travail" vient du latin "trepalium" qui signifie "torture". Voilà, je vous laisse avec ça.</p>
-        <a href="#" class="post-more">Continue reading</a>
-    </article>
-    <article>
-        <span class="category-badge category-badge--news">News</span>
-        <h3 class="post-title">Les chats augmentent la productivité</h3>
-        <div class="meta">
-            <img src="../images/icon-tre.png" alt="">
-            <span><strong>Croco miaou</strong> le 27 Septembre 2019</span>
-        </div>
-        <p class="excerpt">Jusqu'à un certain nombre. Au delà de 28 chats, les scientifiques semblent quelque peu sceptiques. Surtout sur l'aspect budgétaire...</p>
-        <a href="#" class="post-more">Continue reading</a>
-    </article>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article>
+            <?php
+            $categories = get_the_category();
+            foreach ($categories as $category):
+            ?>
+            <span class="category-badge category-badge--<?= $category->slug ?>"><?= $category->name ?></span>
+            <?php endforeach; ?>
+            <h3 class="post-title"><?php the_title(); ?></h3>
+            <div class="meta">
+                <?php echo get_avatar(get_the_author_meta('id'), 32); ?>
+                <span><strong><?php the_author(); ?></strong> le <?= get_the_date(); ?></span>
+            </div>
+            <p class="excerpt"><?php the_excerpt(); ?></p>
+            <a href="<?php the_permalink(); ?>" class="post-more">Continue reading</a>
+        </article>
+    <?php endwhile; else : ?>
+        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
